fceaa5c823cc9cf0514317dfffd4ee64eb0e1a74
Dmitry Orlov <luckenty@yandex.ru>
2016-08-31 16:04:18 +0300

    добавил строку, проверяю настройки

--- src/DimansApps/HighLowElement.java
+++ src/DimansApps/HighLowElement.java
@@ -4,4 +4,8 @@ package DimansApps;
  * Created by Администратор on 31.08.2016.
  */
 public class HighLowElement {
+ public static void printing(String srgs[]) {
+        System.out.println("ВСЕГО ХОРОШЕГО, РУССКИЕ ИДУТ");
+        System.out.println("раз два три четыре пять");
+    }
 }
